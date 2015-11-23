6f5942fb382b9073a00fd77f022a176f181ac075
Connor McLaughlin <mclaughc@outlook.com>
2015-11-23 17:57:28 +1000

    main: some more sensible defaults

--- src/main.cpp
+++ src/main.cpp
@@ -385,7 +385,10 @@ static bool ParseArguments(int argc, char *argv[], ProgramArgs *out_args)
     out_args->bios_filename = nullptr;
     out_args->cart_filename = nullptr;
     out_args->disable_bios = false;
-    out_args->permissive_memory = true;
+    out_args->permissive_memory = false;
+    out_args->accurate_timing = true;
+    out_args->frame_limiter = true;
+    out_args->enable_audio = true;
 
     for (int i = 1; i < argc; i++)
     {
